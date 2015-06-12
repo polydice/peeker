1e4eb77d5bd9ee33272b7974506c22ff9631b369
frozenfung <frozenfung@gmail.com>
2015-06-12 11:35:28 +0800

    Optimize syntax

--- app/assets/javascripts/peeker.js
+++ app/assets/javascripts/peeker.js
@@ -1,47 +1,46 @@
 (function () {
 
+  // Find peek element
   var peekElement = document.querySelector("#peek");
 
   // Check existence of peek element
-  if (!peekElement) {
+  if (!peekElement) 
     return false;
-  } else {
-    // Listener Class
-    var Listener = function (target, eventHandler) {
-      this.target = new RegExp(target, "i");
-      this.queue = "";
-      this.eventHandler = eventHandler;
-      this.compare = function () {
-        if (this.queue.match(this.target)) {
-          this.queue = "";
-          this.eventHandler();
-        }
-      };
+  
+  // Listener Class
+  var Listener = function (target, handler) {
+    this.target = new RegExp(target, "i");
+    this.queue = "";
+    this.handler = handler;
+    this.compare = function () {
+      if (this.queue.match(this.target)) {
+        this.queue = "";
+        this.handler();
+      }
     };
+  };
 
-    // Target
-    var target = "peek";
+  // Target
+  var target = "peek";
 
-    // Handler
-    var handler = function () {
-      peekElement.style.display = (localStorage.getItem("peekStatus") == "none") ? "block" : "none";
-      localStorage.setItem("peekStatus", peekElement.style.display);
-    };
+  // Handler
+  var handler = function () {
+    peekElement.style.display = (localStorage.getItem("peekStatus") == "none") ? "block" : "none";
+    localStorage.setItem("peekStatus", peekElement.style.display);
+  };
 
-    // Access peekStatus with localStorage
-    if (!localStorage.getItem("peekStatus")) {
-      localStorage.setItem("peekStatus", "none");
-    }
+  // Set peekStatus with localStorage
+  if (!localStorage.getItem("peekStatus")) 
+    localStorage.setItem("peekStatus", "none");
 
-    // Give born a Listener object
-    var peekListener = new Listener(target, handler);
-    peekElement.style.display = localStorage.getItem("peekStatus");
+  // Give born to a Listener object
+  var peekListener = new Listener(target, handler);
+  peekElement.style.display = localStorage.getItem("peekStatus");
 
-    // Add listener for window
-    window.addEventListener("keydown", function (e) {
-      peekListener.queue = peekListener.queue.concat(String.fromCharCode(e.keyCode));
-      peekListener.compare();
-    });
-  }
+  // Hook listener on window
+  window.addEventListener("keydown", function (e) {
+    peekListener.queue = peekListener.queue.concat(String.fromCharCode(e.keyCode));
+    peekListener.compare();
+  });
 
 })();
